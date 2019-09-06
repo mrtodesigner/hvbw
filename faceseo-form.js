b602add75c32f30acc39d6d37faa95c0b8022625
mrtodesigner <mrtodesigner@gmail.com>
2019-09-06 18:29:50 +0700

    Update faceseo-form.js

--- faceseo-form.js
+++ faceseo-form.js
@@ -139,7 +139,7 @@ jQuery("#myModalthanh").html(formdiv);
 function thanhxet(){
 
 if(submittedform) {
-	window.location="https://qno.vn";
+	window.location="https://faceseo.vn";
 var linkhientai=window.location;
 if(getCookie(linkhientai)){
                 	
